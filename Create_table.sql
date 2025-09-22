c93254cca142cc704227fbce511e2b18f7b8b696
TEHill1910 <thomas_e_hill@outlook.com>
2025-09-22 12:30:16 +0100

    Create Create_table.sql
    Initial draft of create table

---
+++ Create_table.sql
@@ -0,0 +1,39 @@
+
+CREATE TABLE (
+ID INT,
+Purchase-date TIMESTAMPTZ,
+Date DATE,
+Time TIME,
+Quarter VARCHAR(10),
+Qtr INT,
+YEAR INT,
+MONTH INT,
+Date2 INT,
+"Special DAY" BOOL, -- 1 OR 0, TO CONFIRM
+Online Sale Offers BOOL, -- 1 OR 0, TO CONFIRM
+DAY INT,
+Weekend BOOL, -- 1 OR 0, TO CONFIRM
+Morning BOOL, -- 1 OR 0, TO CONFIRM
+Afternoon BOOL, -- 1 OR 0, TO CONFIRM
+Evening BOOL, -- 1 OR 0, TO CONFIRM
+Night BOOL, -- 1 OR 0, TO CONFIRM
+Gender BOOL, -- 1 OR 0, TO CONFIRM
+Customer ID VARCHAR (10),
+Gender (F=1665, M=1815) VARCHAR(10), -- CHAR? TO CONFIRM
+Product-Name VARCHAR(180),
+Item-Status VARCHAR(10),
+Quantity INT,
+Currency VARCHAR(10),
+Item Price FLOAT,
+Shipping-Price FLOAT,
+Ship-City VARCHAR(20),
+Ship-State VARCHAR(20),
+Ship-Postal-Code VARCHAR(10),
+Category VARCHAR(20),
+Total_amount FLOAT,
+Author VARCHAR(30),
+PUBLICATION VARCHAR(30),
+Profit Percentage (%) INT,
+Profit (INR) FLOAT,
+Cost Price FLOAT
+);
