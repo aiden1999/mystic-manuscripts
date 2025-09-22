33c33b60d00cf3d35a81e11cae82b413bcaa7e3f
TEHill1910 <thomas_e_hill@outlook.com>
2025-09-22 15:31:40 +0100

    Update Create_table_ideal.sql
    Updated for V2

--- Create_table_ideal.sql
+++ Create_table_ideal.sql
@@ -1,36 +1,36 @@
 CREATE TABLE all_2509."select_copy" (
-	id INT,
+	id SERIAL PRIMARY KEY,
 	purchase_date TIMESTAMPTZ,
 	date DATE,
 	time TIME,
-	quarter VARCHAR(10),
+	quarter VARCHAR(100),
 	qtr INT,
 	year INT,
 	month INT,
-	date2 INT,
+	day INT,
 	special_day BOOL, -- 1 OR 0, TO CONFIRM
 	online_sale_offers BOOL, -- 1 OR 0, TO CONFIRM
-	day INT,
+	day_of_week INT,
 	weekend BOOL, -- 1 OR 0, TO CONFIRM
 	morning BOOL, -- 1 OR 0, TO CONFIRM
 	afternoon BOOL, -- 1 OR 0, TO CONFIRM
 	evening BOOL, -- 1 OR 0, TO CONFIRM
 	night BOOL, -- 1 OR 0, TO CONFIRM
 	gender_bool BOOL, -- 1 OR 0, TO CONFIRM
-	customer_id VARCHAR (10),
-	gender VARCHAR(10), -- CHAR? TO CONFIRM
+	customer_id VARCHAR (100),
+	gender VARCHAR(100), -- CHAR? TO CONFIRM
 	product_name VARCHAR(180),
-	item_status VARCHAR(10),
+	item_status VARCHAR(100),
 	quantity INT,
-	currency VARCHAR(10),
+	currency VARCHAR(100),
 	item_price FLOAT,
 	shipping_price FLOAT,
-	ship_city VARCHAR(20),
-	ship_state VARCHAR(20),
-	ship_postal_code VARCHAR(10),
+	ship_city VARCHAR(100),
+	ship_state VARCHAR(100),
+	ship_postal_code VARCHAR(100),
 	category VARCHAR(20),
 	total_amount FLOAT,
-	author VARCHAR(30),
+	author VARCHAR(100),
 	publication VARCHAR(100),
 	profit_percentage INT,
 	profit_inr FLOAT,
@@ -45,4 +45,12 @@ GRANT DELETE, INSERT,  REFERENCES, SELECT, UPDATE, TRUNCATE, TRIGGER ON TABLE al
 GRANT DELETE, INSERT,  REFERENCES, SELECT, UPDATE, TRUNCATE, TRIGGER ON TABLE all_2509."select_copy" TO de14_aite;
 
 
+SELECT *
+FROM select_copy
+
+
+SELECT COUNT(*)
+FROM information_schema.columns
+WHERE table_schema = 'all_2509'
+  AND table_name   = 'select_copy';
 
