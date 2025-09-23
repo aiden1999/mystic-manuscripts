ef573a284ebf0ac0fd00099005a48efb48b33b5f
Aiden Tempest <aidentempest99@gmail.com>
2025-09-23 11:00:25 +0100

    updated additional queries

--- additional_queries.sql
+++ additional_queries.sql
@@ -1,6 +1,11 @@
-SELECT 
-	count(id)
-FROM all_2509."select_test"
-WHERE (day_of_week = 6 OR day_of_week = 7)
-	AND weekend != 1;
+SELECT
+    COUNT(id)
+FROM all_2509.select_test
+WHERE
+    (day_of_week = 6 OR day_of_week = 7) AND weekend != 1;
 
+SELECT COUNT(id)
+FROM
+    all_2509.select_test
+WHERE
+    cost_price <= 0 OR profit_inr <= 0;
